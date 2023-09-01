b088569f541cc0852a391ef995324c5be4c2673a
CatUwUKitty <85913557+CatUwUKitty@users.noreply.github.com>
2023-09-01 12:13:02 +0800

    Update CPUSaver.lua

--- CPUSaver.lua
+++ CPUSaver.lua
@@ -1,5 +1,7 @@
 while task.wait() do
     local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
     local a={[1]="NextLevel"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+    local a={[1]="Next_Level"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+    local a={[1]="next_level"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
     wait(5)
 end
