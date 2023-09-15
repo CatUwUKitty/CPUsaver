a14c1b256ebb66e4c58fdffef63ed55ca747a3d7
CatUwUKitty <85913557+CatUwUKitty@users.noreply.github.com>
2023-09-15 16:49:25 +0800

    Update CPUSaver.lua

--- CPUSaver.lua
+++ CPUSaver.lua
@@ -1,9 +1,9 @@
-queue_on_teleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/CatUwUKitty/CPUsaver/main/CPUSaver.lua"))()")
-while task.wait(5) do
-    local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
-    local a={[1]="NextLevel"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
-    local a={[1]="Next_Level"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
-    local a={[1]="next_level"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
-    warn("Next Level")
-    wait(5)
-end
+--queue_on_teleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/CatUwUKitty/CPUsaver/main/CPUSaver.lua"))()")
+--while task.wait(5) do
+--    local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+--    local a={[1]="NextLevel"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+--    local a={[1]="Next_Level"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+--    local a={[1]="next_level"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+--    warn("Next Level")
+--    wait(5)
+--end
